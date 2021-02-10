f8e2ee7ff0e7539b885137da66afc917ac754739
salehi-s <salehi.s@husky.neu.edu>
2021-02-09 23:31:02 -0500

    Add Create button with add dummy user functionality and Remove button with temporary console.log() statements

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -16,29 +16,50 @@
     function findUserById() { … } // optional - might not need this
 })()*/
 
-var users = [
+var tableBody = jQuery("tbody")
+
+var userArrayDummy = [
     {username: 'lovelace.a', password: String.fromCharCode(160), firstName: 'Ada', lastName: 'Lovelace', role: 'FACULTY'},
     {username: 'liddell.a', password: String.fromCharCode(160), firstName: 'Alice', lastName: 'Liddell', role: 'STUDENT'},
     {username: 'salehi.s', password: String.fromCharCode(160), firstName: 'Sina', lastName: 'Salehi', role: 'ADMIN'}
 ]
 
-var tableBody = jQuery("tbody")
+var userDummy = {username: 'dummy.u', password: String.fromCharCode(160), firstName: 'User', lastName: 'Dummy', role: 'STUDENT'}
 
-function renderUsers(table, users) {
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i]
-        table.append(`<tr className="wbdv-template wbdv-user wbdv-hidden">
-                              <td className="wbdv-username">${user.username}</td>
-                              <td>${user.password}</td>
-                              <td className="wbdv-first-name">${user.firstName}</td>
-                              <td className="wbdv-last-name">${user.lastName}</td>
-                              <td className="wbdv-role">${user.role}</td>
-                              <td className="wbdv-actions"><span className="pull-right">
-                                  <button className="btn btn-outline-dark"><i className="fa-2x fa fa-times wbdv-remove"> Remove</i></button>
-                                  <button className="btn btn-outline-dark"><i className="fa-2x fa fa-pencil wbdv-edit"> Edit</i></button>
-                              </span></td>
-                          </tr>`)
-    }
+var createButton = jQuery(".wbdv-button-create")
+createButton.click(function () {addUser(tableBody, userArrayDummy, userDummy)})
+
+var updateButton = jQuery(".wbdv-button-update")
+
+function addUser(table, userArray, user) {
+    userArray.push(user)
+    renderUsers(table, userArray)
 }
 
-renderUsers(tableBody, users)
+addUser(tableBody, userArrayDummy, {username: 'lovelace.a', password: String.fromCharCode(160), firstName: 'Ada', lastName: 'Lovelace', role: 'FACULTY'})
+
+function renderUsers(table, userArray) {
+    table.empty()
+    for (var i = 0; i < userArray.length; i++) {
+        var user = userArray[i]
+        table.prepend(`<tr class="wbdv-template wbdv-user wbdv-hidden">
+                           <td class="wbdv-username">${user.username}</td>
+                           <td>${user.password}</td>
+                           <td class="wbdv-first-name">${user.firstName}</td>
+                           <td class="wbdv-last-name">${user.lastName}</td>
+                           <td class="wbdv-role">${user.role}</td>
+                           <td class="wbdv-actions"><span class="pull-right">
+                               <button class="btn btn-outline-dark wbdv-button-remove" id="${i}"><i class="fa-2x fa fa-times wbdv-remove"> Remove</i></button>
+                               <button class="btn btn-outline-dark wbdv-button-edit"><i class="fa-2x fa fa-pencil wbdv-edit"> Edit</i></button>
+                           </span></td>
+                       </tr>`)
+    }
+    jQuery(".wbdv-remove").click(function (event) {
+        console.log(event.target)
+        var removeButton = jQuery(event.target)
+        var rBClass = removeButton.attr("class")
+        console.log(rBClass)
+    })
+}
+
+renderUsers(tableBody, userArrayDummy)
