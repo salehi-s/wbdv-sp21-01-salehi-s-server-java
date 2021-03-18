d3f9690f24e9d6b55f4867afdcc965c3fbb4f5c7
salehi-s <salehi.s@husky.neu.edu>
2021-03-17 21:21:17 -0400

    Add Widget (init) and declare private attributes

---
+++ src/main/java/com/example/wbdvsp2103salehisserverjava/models/Widget.java
@@ -0,0 +1,18 @@
+package com.example.wbdvsp2103salehisserverjava.models;
+
+public class Widget {
+  private String name;
+  private Integer id;
+  private String type;
+  private Integer widgetOrder;
+  private String text;
+  private String src;
+  private String url;
+  private String href;
+  private Integer size;
+  private Integer width;
+  private Integer height;
+  private String cssClass;
+  private String style;
+  private String value;
+}
