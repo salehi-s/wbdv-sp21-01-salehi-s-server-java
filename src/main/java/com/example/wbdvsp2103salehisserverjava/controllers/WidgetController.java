6bf13d0fe38c51d102fd9fbd778bd10b7734a5b7
salehi-s <salehi.s@husky.neu.edu>
2021-03-20 00:01:49 -0400

    Add CrossOrigin Access to Widget Controller

--- src/main/java/com/example/wbdvsp2103salehisserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2103salehisserverjava/controllers/WidgetController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
   @Autowired
   WidgetService service;
